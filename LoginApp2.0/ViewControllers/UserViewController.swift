4ac190638d9e58bc0e54854b6f612adc4e7a4923
Andrey Sokolov <hi@andreysokolov.com>
2022-03-14 00:48:57 +0300

    Add New View Controller

---
+++ LoginApp2.0/ViewControllers/UserViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UserViewController.swift
+//  LoginApp2.0
+//
+//  Created by Andrey on 14.03.2022.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
