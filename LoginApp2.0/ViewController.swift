dc4d430ef95ee4d9e5c2e331423d7cd1155f6b5e
Andrey Sokolov <hi@andreysokolov.com>
2022-03-13 23:29:24 +0300

    Initial Commit

---
+++ LoginApp2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp2.0
+//
+//  Created by Andrey on 13.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
